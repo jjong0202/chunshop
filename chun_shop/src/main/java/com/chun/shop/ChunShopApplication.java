ca5561bf219498f02105d1739f0216850b42f230
jjong0202 <jjong0202@jjong>
2019-09-20 13:25:07 +0900

    first commit

---
+++ chun_shop/src/main/java/com/chun/shop/ChunShopApplication.java
@@ -0,0 +1,13 @@
+package com.chun.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunShopApplication.class, args);
+	}
+
+}
